b2a58c3c8b39222ee9e80d1c940b1b0535ea1375
Bandar1414 <bandor141414@gmail.com>
2026-07-11 14:41:01 +0000

    Complete functions nested loops tasks 0 to 11

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The result of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
