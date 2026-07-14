f3e1168e1fbfafe039b18432be4e1358583f029f
Bandar <bandor141414@gmail.com>
2026-07-14 16:55:37 +0000

    Add calculator skeleton

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+	{
+			int choice;
+				printf("Simple Calculator\n");
+				do
+				{
+				printf("1) Add\n");
+				printf("2) Subtract\n");
+				printf("3) Multiply\n");
+				printf("4) Divide\n");
+				printf("0) Quit\n");
+				printf("Choice: ");
+				scanf("%d", &choice);
+				if (choice < 0 || choice > 4)
+				{
+							printf("Invalid choice\n");
+						}
+				} while (choice != 0);
+				printf("Bye!\n");
+				return (0);
+			}
